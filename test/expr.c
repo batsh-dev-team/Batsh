c58e18de83bfbba8f22eed513895bc2f704a1a3e
BYVoid <byvoid@byvoid.com>
2013-09-11 10:34:15 +0100

    Test cases

---
+++ test/expr.c
@@ -0,0 +1,4 @@
+a = "b" ++ 1+(4+6)*3;
+b = whoami();
+c = "echo";
+c(a, b, date());
---
+++ test/if.c
@@ -0,0 +1,18 @@
+if (2 < 10) {
+	echo("He");
+}
+
+if (true) {
+	if(false){
+		v=(4 + 1 );
+	} else {
+		v = 2;
+	}
+} else {
+
+}
+echo(v);
+
+if (1 == 12) {
+	echo("yes");
+}
