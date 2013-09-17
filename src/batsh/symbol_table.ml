cc6aee2726e37742c702318cf3690559288469c7
BYVoid <byvoid@byvoid.com>
2013-09-17 21:14:46 +0100

    Store variables separately in symbol table

--- src/batsh/symbol_table.ml
+++ src/batsh/symbol_table.ml
@@ -1,58 +1,58 @@
 open Core.Std
 open Batshast
 
-type symtable_entry =
-  | Variable of variable_entry
-  | Function of function_entry
+type variable_entry = string
 
-and variable_entry = {
-  name : string;
-  scope : function_entry option;
+type variable_table = (string, variable_entry) Hashtbl.t
+
+type function_entry =
+  | Declare
+  | Define of variable_table
+
+type t = {
+  functions : (string, function_entry) Hashtbl.t;
+  globals : variable_table;
 }
 
-and function_entry = string
-
-type t = (string, symtable_entry) Hashtbl.t
-
 let rec process_leftvalue
-    (symtable: t)
-    (scope: function_entry option)
+    (scope: variable_table)
     (lvalue: leftvalue) =
   match lvalue with
   | Identifier ident ->
-    (* TODO local and global *)
-    Hashtbl.change symtable ident (fun original ->
-        Some (Variable {
-            name = ident;
-            scope;
-          })
-      );
-  | ListAccess (lvalue, _) -> process_leftvalue symtable scope lvalue
+    Hashtbl.change scope ident (fun _ -> Some ident)
+  | ListAccess (lvalue, _) ->
+    process_leftvalue scope lvalue
 
 let process_statement
-    (symtable: t)
-    (scope: function_entry option)
+    (scope: variable_table)
     (stmt: statement) =
   match stmt with
-  | Assignment (lvalue, _) -> process_leftvalue symtable scope lvalue
+  | Assignment (lvalue, _) -> process_leftvalue scope lvalue
   | _ -> ()
 
 let process_toplevel (symtable: t) (topl: toplevel) =
   match topl with
-  | Statement stmt -> process_statement symtable None stmt
+  | Statement stmt -> process_statement symtable.globals stmt
   | Function (name, _, stmts) ->
-    Hashtbl.change symtable name (fun original ->
-        (* TODO duplicate *)
-        Some (Function name)
-      );
-    List.iter stmts ~f: (process_statement symtable (Some name))
+    match Hashtbl.find symtable.functions name with
+    | Some _ -> () (* TODO duplicate *)
+    | None ->
+      let variables = Hashtbl.create ~hashable: String.hashable () in
+      Hashtbl.change symtable.functions name (fun original ->
+          (* TODO declaration *)
+          Some (Define variables)
+        );
+      List.iter stmts ~f: (process_statement variables)
 
 let create (ast: asttype) :t =
-  let symtable = Hashtbl.create ~hashable: String.hashable () in
+  let symtable = {
+    functions = Hashtbl.create ~hashable: String.hashable ();
+    globals = Hashtbl.create ~hashable: String.hashable ()
+  } in
   List.iter ast ~f: (process_toplevel symtable);
   symtable
 
 let find_function (symtable: t) (name: string) :bool =
-  match Hashtbl.find symtable name with
+  match Hashtbl.find symtable.functions name with
   | Some _ -> true
   | None -> false
