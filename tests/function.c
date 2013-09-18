bc2b21076172aa00260462b53bacc38fb1d35a98
BYVoid <byvoid@byvoid.com>
2013-09-18 16:57:53 +0100

    Update function unit test: global variable

--- tests/function.c
+++ tests/function.c
@@ -1,5 +1,6 @@
 v1 = "Global V1";
 v2 = "Global V2";
+v3 = "Global V3";
 
 function func1(p1, p2) {
   echo(p1, p2);
@@ -9,8 +10,12 @@ function func2(p) {
   v1 = "Local " ++ p;
   echo(v1);
   echo(v2);
+  global v3;
+  v3 = "V3 Modified.";
 }
 
 func1("Hello", "World");
 func2("Var");
-echo(v1, p);
+echo(v1);
+echo(p);
+echo(v3);
