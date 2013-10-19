6f3b4b4331b3347d466213f30869fd2417efb745
BYVoid <byvoid@byvoid.com>
2013-10-19 15:03:08 +0800

    Unit test for Windows Batch.

--- src/test.ml
+++ src/test.ml
@@ -1,40 +1,78 @@
 open Core.Std
+open Unix.Process_channels
 open OUnit
 
-let test_run_bash name =
-  let filename = name ^ ".c" in
-  let batsh = Parser.create_from_file filename in
-  let bash = Bash.compile batsh in
+let drop_carrage_return str =
+  let buffer = Buffer.create (String.length str) in
+  String.iter str ~f:(fun ch ->
+      if ch <> '\r' then
+        Buffer.add_char buffer ch
+    );
+  Buffer.contents buffer
 
-  let inx, outx = Unix.open_process "bash" in
-  let code = Bash.print bash in
-  Out_channel.output_string outx code;
-  Out_channel.close outx;
-  let output = In_channel.input_all inx in
-  In_channel.close inx;
-  let exit_status = Unix.close_process (inx, outx) in
+let test_result expected output exit_status =
   let exit_message = Unix.Exit_or_signal.to_string_hum exit_status in
   assert_equal "exited normally" exit_message ~printer: Fn.id;
+  assert_equal expected output ~printer: Fn.id
 
+let test_bash batsh expected =
+  let bash = Bash.compile batsh in
+  let code = Bash.print bash in
+  let stdout, stdin = Unix.open_process "bash" in
+  Out_channel.output_string stdin code;
+  Out_channel.close stdin;
+  let output = In_channel.input_all stdout in
+  let exit_status = Unix.close_process (stdout, stdin) in
+  test_result expected output exit_status
+
+let test_winbat batsh expected =
+  let winbat = Winbat.compile batsh in
+  let code = Winbat.print winbat in
+  let filename = Filename.temp_file "batsh" ".bat" in
+  let outx = Out_channel.create filename in
+  Out_channel.output_string outx code;
+  Out_channel.close outx;
+
+  let cmd = "wine cmd /c " ^ filename in
+  let channels = Unix.open_process_full cmd ~env:[||] in
+  let output_raw = In_channel.input_all channels.stdout in
+  let output = drop_carrage_return output_raw in
+  let exit_status = Unix.close_process_full channels in
+  test_result expected output exit_status
+
+let get_expected name =
   let answer_filename = "output/" ^ name ^ ".txt" in
   let inx = In_channel.create answer_filename in
-  let answer = In_channel.input_all inx in
+  let expected = In_channel.input_all inx in
   In_channel.close inx;
-  assert_equal answer output ~printer: Fn.id
+  expected
 
-let test_bash name _ =
-  test_run_bash name
+let test name func _ =
+  let expected = get_expected name in
+  let filename = name ^ ".c" in
+  let batsh = Parser.create_from_file filename in
+  func batsh expected
 
-let test_cases = "Batsh Unit Tests" >:::
-                   ["Block" >:: test_bash "block";
-                    "Arith" >:: test_bash "arith";
-                    "Assignment" >:: test_bash "assignment";
-                    "Array" >:: test_bash "array";
-                    "Expressions" >:: test_bash "expr";
-                    "If" >:: test_bash "if";
-                    "While" >:: test_bash "while";
-                    "Function" >:: test_bash "function";
-                    "Recursion" >:: test_bash "recursion"]
+let test_cases = "Batsh Unit Tests" >::: [
+    "[Bash]Block"         >:: test "block" test_bash;
+    "[Bash]Arith"         >:: test "arith" test_bash;
+    "[Bash]Assignment"    >:: test "assignment" test_bash;
+    "[Bash]Array"         >:: test "array" test_bash;
+    "[Bash]Expressions"   >:: test "expr" test_bash;
+    "[Bash]If"            >:: test "if" test_bash;
+    "[Bash]While"         >:: test "while" test_bash;
+    "[Bash]Function"      >:: test "function" test_bash;
+    "[Bash]Recursion"     >:: test "recursion" test_bash;
+    "[Winbat]Block"       >:: test "block" test_winbat;
+    (* "[Winbat]Arith"       >:: test "arith" test_winbat; *)
+    "[Winbat]Assignment"  >:: test "assignment" test_winbat;
+    (* "[Winbat]Array"       >:: test "array" test_winbat; *)
+    (* "[Winbat]Expressions" >:: test "expr" test_winbat; *)
+    (* "[Winbat]If"          >:: test "if" test_winbat; *)
+    (* "[Winbat]While"       >:: test "while" test_winbat; *)
+    (* "[Winbat]Function"    >:: test "function" test_winbat; *)
+    (* "[Winbat]Recursion"   >:: test "recursion" test_winbat; *)
+  ]
 
 let _ =
   run_test_tt_main test_cases
