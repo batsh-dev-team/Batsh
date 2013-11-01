54fe4baf4375a4f563697dede9c3de27313ab4fc
Carbo Kuo <byvoid1@gmail.com>
2013-11-02 00:49:42 +0800

    Fix update.js.

--- scripts/update.js
+++ scripts/update.js
@@ -5,10 +5,10 @@ var files = fs.readdirSync("tests");
 for (var i = 0; i < files.length; i++) {
   var file = files[i];
   var ext = path.extname(file);
-  if (ext == '.c') {
+  if (ext == '.js') {
     var name = path.basename(file, ext);
-    console.log('./main.byte bash tests/' + name + '.c > tests/bash/' + name + '.sh');
-    console.log('./main.byte bat tests/' + name + '.c > tests/batch/' + name + '.bat');
+    console.log('./main.byte bash tests/' + name + '.js > tests/bash/' + name + '.sh');
+    console.log('./main.byte winbat tests/' + name + '.js > tests/batch/' + name + '.bat');
     console.log('bash tests/bash/' + name + '.sh > tests/output/' + name + '.txt');
   }
 }
