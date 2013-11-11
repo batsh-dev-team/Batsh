8b04d96617122111d45a759cf0d3042a21b1ecde
Carbo Kuo <byvoid1@gmail.com>
2013-11-11 18:11:47 +0800

    Check if qualified `global` is defined in a function

--- src/semantic_checker.ml
+++ src/semantic_checker.ml
@@ -1,6 +1,15 @@
 open Core_kernel.Std
+open Batsh_ast
 
 exception Error of string
 
-let check (ast : Batsh_ast.t) : unit =
-  ()
+let check_toplevel (topl: toplevel) =
+  match topl with
+  | Statement (Global _) ->
+    raise (Error "qualifier 'global' must be used in a function")
+  | Statement _
+  | Function _ ->
+    ()
+
+let check (ast : t) : unit =
+  List.iter ast ~f: check_toplevel
