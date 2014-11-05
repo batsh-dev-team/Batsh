069b7af1e20d43c1d3a0358d4d50a67ec76b4061
Carbo Kuo <byvoid@byvoid.com>
2014-11-05 22:50:01 +0100

    Make Generator polymorphic

--- library/Batsh/Generator.hs
+++ library/Batsh/Generator.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Batsh.Generator where
 
 import qualified Data.ByteString
@@ -11,98 +12,110 @@ import Data.ByteString.Builder(Builder,
                                stringUtf8,
                                toLazyByteString)
 
-import Batsh.Ast
+import Batsh.Ast.Poly
 
-renderLiteral :: Literal -> Builder
-renderLiteral literal = case literal of
-  Int num _ -> intDec num
-  Float num _ -> floatDec num
-  String str _ -> mconcat [charUtf8 '"',
-                           stringUtf8 str,
-                           charUtf8 '"']
-  Bool bool _ -> case bool of
-    True -> stringUtf8 "true"
-    False -> stringUtf8 "false"
-  List list _ -> mconcat [stringUtf8 "[",
-                          renderExpressions list,
-                          stringUtf8 "]"]
-
-renderLeftValue :: LeftValue -> Builder
-renderLeftValue lvalue = case lvalue of
-  Identifier ident _ -> stringUtf8 ident
-  ListAccess (lvalue, expr) _ -> mconcat [renderLeftValue lvalue,
-                                          stringUtf8 "[",
-                                          renderExpression expr,
-                                          stringUtf8 "]"]
-
--- Render a subexpression. Add parenthesis if and only if necessary.
-renderSubExpression :: (Operator a) => a -> Expression -> Builder
-renderSubExpression operator subExpr =
-  let rendered = renderExpression subExpr in
-  let renderedWithParen = mconcat [charUtf8 '(', rendered, charUtf8 ')'] in
-  case subExpr of
-    -- if subexpression is a binary expression and the precedence of operator is
-    -- lower, then add (). E.g. + is less precedent than *.
-    Binary (subOperator, _, _) _
-      | precedence subOperator < precedence operator ->
-         renderedWithParen
-    Unary (subOperator, _) _
-      | precedence subOperator < precedence operator ->
-         renderedWithParen
-    _ -> rendered
-
-renderUnary :: (UnaryOperator, Expression) -> Builder
-renderUnary (operator, expr) =
-  mconcat [stringUtf8 $ operatorStr operator,
-           renderSubExpression operator expr]
-
-renderBinary :: (BinaryOperator, Expression, Expression) -> Builder
-renderBinary (operator, left, right) =
-  mconcat [renderSubExpression operator left,
-           charUtf8 ' ',
-           stringUtf8 $ operatorStr operator,
-           charUtf8 ' ',
-           renderSubExpression operator right]
-
-renderExpression :: Expression -> Builder
-renderExpression expr = case expr of
-  LeftValue lvalue _ -> renderLeftValue lvalue
-  Literal literal _ -> renderLiteral literal
-  Unary unary _ -> renderUnary unary
-  Binary binary _ -> renderBinary binary
-  Assign (lvalue, expr) _ -> mconcat [renderLeftValue lvalue,
-                                      stringUtf8 " = ",
-                                      renderExpression expr]
-  Call (ident, exprs) _ -> mconcat [stringUtf8 ident,
-                                    charUtf8 '(',
-                                    renderExpressions exprs,
-                                    charUtf8 ')']
+class Renderable a where
+  render :: a -> Builder
 
 renderSeparateList :: [a] -> String -> (a -> Builder) -> Builder
 renderSeparateList list separator renderer = case list of
   [] -> mempty
   [elem] -> renderer elem
   (elem : rest) -> mconcat [renderer elem,
-                            stringUtf8 separator,
+                            render separator,
                             renderSeparateList rest separator renderer]
 
-renderExpressions :: [Expression] -> Builder
-renderExpressions exprs = renderSeparateList exprs ", " renderExpression
-
 renderIndention :: Int -> Builder
-renderIndention level = stringUtf8 (take level $ repeat ' ')
+renderIndention level = render (take level $ repeat ' ')
 
-renderBlock :: [Statement] -> Int -> Builder
+instance Renderable Char where
+  render char = charUtf8 char
+
+instance Renderable String where
+  render str = stringUtf8 str
+
+instance Renderable (PLiteral a) where
+  render literal = case literal of
+    Int num _ -> intDec num
+    Float num _ -> floatDec num
+    String str _ -> mconcat [render '"',
+                             render str,
+                             render '"']
+    Bool bool _ -> case bool of
+      True -> render "true"
+      False -> render "false"
+    List list _ -> mconcat [render "[",
+                            render list,
+                            render "]"]
+
+instance Renderable (PLeftValue a) where
+  render lvalue = case lvalue of
+    Identifier ident _ -> render ident
+    ListAccess (lvalue, expr) _ -> mconcat [render lvalue,
+                                            render "[",
+                                            render expr,
+                                            render "]"]
+
+instance Renderable (PExpression a) where
+  render expr = case expr of
+    LeftValue lvalue _ -> render lvalue
+    Literal literal _ -> render literal
+    Unary unary _ -> renderUnary unary
+    Binary binary _ -> renderBinary binary
+    Assign (lvalue, expr) _ -> mconcat [render lvalue,
+                                        render " = ",
+                                        render expr]
+    Call (ident, exprs) _ -> mconcat [render ident,
+                                      render '(',
+                                      render exprs,
+                                      render ')']
+    where
+    -- Render a subexpression. Add parenthesis if and only if necessary.
+    renderSubExpression :: (Operator a) => a -> (PExpression b) -> Builder
+    renderSubExpression operator subExpr =
+      let rendered = render subExpr in
+      let renderedWithParen = mconcat [render '(', rendered, render ')'] in
+      case subExpr of
+        -- if subexpression is a binary expression and the precedence of operator is
+        -- lower, then add (). E.g. + is less precedent than *.
+        Binary (subOperator, _, _) _
+          | precedence subOperator < precedence operator ->
+             renderedWithParen
+        Unary (subOperator, _) _
+          | precedence subOperator < precedence operator ->
+             renderedWithParen
+        _ -> rendered
+
+    renderUnary :: (PUnaryOperator a, PExpression a) -> Builder
+    renderUnary (operator, expr) =
+      mconcat [render $ operatorStr operator,
+               renderSubExpression operator expr]
+
+    renderBinary :: (PBinaryOperator a, PExpression a, PExpression a) -> Builder
+    renderBinary (operator, left, right) =
+      mconcat [renderSubExpression operator left,
+               render ' ',
+               render $ operatorStr operator,
+               render ' ',
+               renderSubExpression operator right]
+
+instance Renderable [PExpression a] where
+  render exprs = renderSeparateList exprs ", " render
+
+instance Renderable (PStatement a) where
+  render stmt = renderStatementIndent stmt 0 False
+
+renderBlock :: [PStatement a] -> Int -> Builder
 renderBlock stmts level = mconcat
-  [stringUtf8 "{\n",
+  [render "{\n",
    renderSeparateList stmts "\n" $
     \stmt -> renderStatementIndent stmt (level + 2) False,
-   stringUtf8 "\n",
+   render "\n",
    renderIndention level,
-   stringUtf8 "}"
+   render "}"
   ]
 
-renderStatementIndent :: Statement -> Int -> Bool -> Builder
+renderStatementIndent :: PStatement a -> Int -> Bool -> Builder
 renderStatementIndent stmt level isClause =
   if isClause then
     renderedStmt
@@ -111,65 +124,62 @@ renderStatementIndent stmt level isClause =
   where
     renderedStmt = case stmt of
       Comment comment _ ->
-        mconcat [stringUtf8 "//",
-                 stringUtf8 comment]
+        mconcat [render "//",
+                 render comment]
       Block stmts _ -> renderBlock stmts level
-      Expression expr _ -> withSemicolon $ renderExpression expr
+      Expression expr _ -> withSemicolon $ render expr
       If (expr, stmt) _ ->
-        mconcat [stringUtf8 "if (",
-                 renderExpression expr,
-                 stringUtf8 ") ",
+        mconcat [render "if (",
+                 render expr,
+                 render ") ",
                  renderClause stmt]
       IfElse (expr, thenStmt, elseStmt) _ ->
-        mconcat [stringUtf8 "if (",
-                 renderExpression expr,
-                 stringUtf8 ") ",
+        mconcat [render "if (",
+                 render expr,
+                 render ") ",
                  renderClause thenStmt,
-                 stringUtf8 " else ",
+                 render " else ",
                  renderClause elseStmt]
       While (expr, stmt) _ ->
-        mconcat [stringUtf8 "while (",
-                 renderExpression expr,
-                 stringUtf8 ") ",
+        mconcat [render "while (",
+                 render expr,
+                 render ") ",
                  renderClause stmt]
       Global ident _ -> withSemicolon $
-        mconcat [stringUtf8 "global ", stringUtf8 ident]
+        mconcat [render "global ", render ident]
       Return (Just expr) _ -> withSemicolon $
-        mconcat [stringUtf8 "return ", renderExpression expr]
-      Return Nothing _ -> withSemicolon $ stringUtf8 "return"
+        mconcat [render "return ", render expr]
+      Return Nothing _ -> withSemicolon $ render "return"
       where
-        withSemicolon builder = mconcat [builder, charUtf8 ';'];
+        withSemicolon builder = mconcat [builder, render ';'];
         renderClause stmt = renderStatementIndent stmt level True
 
-renderStatement :: Statement -> Builder
-renderStatement stmt = renderStatementIndent stmt 0 False
+instance Renderable (PTopLevel a) where
+  render toplevel = case toplevel of
+    Statement stmt _ -> render stmt
+    Function (name, params, stmts) _ ->
+      mconcat [render "function ",
+               render name,
+               render '(',
+               renderSeparateList params ", " render,
+               render ") ",
+               renderBlock stmts 0]
 
-renderTopLevel :: TopLevel -> Builder
-renderTopLevel toplevel = case toplevel of
-  Statement stmt _ -> renderStatement stmt
-  Function (name, params, stmts) _ ->
-    mconcat [stringUtf8 "function ",
-             stringUtf8 name,
-             charUtf8 '(',
-             renderSeparateList params ", " stringUtf8,
-             stringUtf8 ") ",
-             renderBlock stmts 0]
+instance Renderable (PProgram a) where
+  render (Program program _) =
+    mconcat [renderSeparateList program "\n" render,
+             render '\n']
 
-renderProgram :: Program -> Builder
-renderProgram (Program program _) =
-  mconcat [renderSeparateList program "\n" renderTopLevel,
-           charUtf8 '\n']
+generateByteString :: (PProgram a) -> ByteString
+generateByteString program = toLazyByteString $ render program
 
-generateByteString :: Program -> ByteString
-generateByteString program = toLazyByteString $ renderProgram program
-
-generateString :: Program -> String
+generateString :: (PProgram a) -> String
 generateString program = unpack $ generateByteString program
 
-printToStdout :: Program -> IO ()
+printToStdout :: (PProgram a) -> IO ()
 printToStdout program =
   Data.ByteString.Lazy.putStr $ generateByteString program
 
-printToFile :: Program -> FilePath -> IO ()
+printToFile :: (PProgram a) -> FilePath -> IO ()
 printToFile program filename = Data.ByteString.Lazy.writeFile filename code
   where code = generateByteString program
