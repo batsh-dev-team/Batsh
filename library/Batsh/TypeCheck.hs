ae5ca3e91ea4e0f8d184a30123312b64b32d7e9a
Carbo Kuo <byvoid@byvoid.com>
2014-11-07 20:43:22 +0100

    TypeCheck w.i.p

--- library/Batsh/TypeCheck.hs
+++ library/Batsh/TypeCheck.hs
@@ -17,14 +17,39 @@ instance TypeCheckable PLiteral where
     String str pos -> String str (TypeAnno TString pos)
     List exprs pos -> List (typeCheckList exprs) (TypeAnno TList pos)
 
--- All variables are considered as String
 instance TypeCheckable PLeftValue where
   typeCheck literal = case literal of
-    Identifier ident pos -> Identifier ident (TypeAnno TString pos)
+    Identifier ident pos -> Identifier ident (TypeAnno TVaribale pos)
     ListAccess lvalue expr pos ->
-      ListAccess (typeCheck lvalue) (typeCheck expr) (TypeAnno TString pos)
+      ListAccess (typeCheck lvalue) (typeCheck expr) (TypeAnno TVaribale pos)
+
+instance TypeCheckable PUnaryOperator where
+  typeCheck operator = case operator of
+    Not pos -> Not $ TypeAnno TNoType pos
+    Negate pos -> Not $ TypeAnno TNoType pos
+
+instance TypeCheckable PBinaryOperator where
+  typeCheck operator = case operator of
+    Plus pos -> Plus $ TypeAnno TNoType pos
+    Minus pos -> Minus $ TypeAnno TNoType pos
+    Multiply pos -> Multiply $ TypeAnno TNoType pos
+    Divide pos -> Divide $ TypeAnno TNoType pos
+    Modulo pos -> Modulo $ TypeAnno TNoType pos
+    Concat pos -> Concat $ TypeAnno TNoType pos
+    Equal pos -> Equal $ TypeAnno TNoType pos
+    NotEqual pos -> NotEqual $ TypeAnno TNoType pos
+    ArithEqual pos -> ArithEqual $ TypeAnno TNoType pos
+    ArithNotEqual pos -> ArithNotEqual $ TypeAnno TNoType pos
+    Greater pos -> Greater $ TypeAnno TNoType pos
+    Less pos -> Less $ TypeAnno TNoType pos
+    GreaterEqual pos -> GreaterEqual $ TypeAnno TNoType pos
+    LessEqual pos -> LessEqual $ TypeAnno TNoType pos
+    And pos -> And $ TypeAnno TNoType pos
+    Or pos -> Or $ TypeAnno TNoType pos
 
 instance TypeCheckable PExpression where
   typeCheck expr = case expr of
-    LeftValue lvalue pos -> LeftValue (typeCheck lvalue) (TypeAnno TString pos)
-    Literal literal pos -> Literal (typeCheck literal) (TypeAnno TString pos)
+    LeftValue lvalue pos -> LeftValue lvalue' (TypeAnno (nodeType lvalue') pos)
+      where lvalue' = typeCheck lvalue
+    Literal literal pos -> Literal literal' (TypeAnno (nodeType literal') pos)
+      where literal' = typeCheck literal
